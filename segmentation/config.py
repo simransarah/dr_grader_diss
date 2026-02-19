1e4865d2378836f898ef323234e0e9f81683ad86
Simran Sajan <111128218+simransarah@users.noreply.github.com>
2026-02-19 03:30:35 +0000

    Update segmentation configuration parameters

--- segmentation/config.py
+++ segmentation/config.py
@@ -2,7 +2,7 @@ import os
 import torch 
 
 class SegmentationConfig:
-    TARGET_LESION = "ex"
+    TARGET_LESION = "he"
     # paths
     root_dir = os.path.join("/kaggle", "input", "idriddata", "data", "IDRiD", "A. Segmentation")
 
@@ -15,13 +15,13 @@ class SegmentationConfig:
     # model parameters
     backbone = "efficientnet-b3"
     pretrained = True
-    patch_size = (256, 256)
+    patch_size = (128, 128)
     in_channels = 3  
     out_channels = 1
     batch_size = 4   
     virtual_batch_size = 16 
     learning_rate = 1e-4
-    num_epochs = 150
+    num_epochs = 100
     num_classes = 3
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
