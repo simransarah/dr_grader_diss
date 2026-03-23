eccb13a0458f0dec47ee127a2041fc2b7c58d40c
Simran Sajan <111128218+simransarah@users.noreply.github.com>
2026-03-23 17:31:47 +0000

    Refactor SegmentationConfig with additional paths

--- segmentation/config.py
+++ segmentation/config.py
@@ -1,17 +1,20 @@
 import os 
 import torch 
-
+ 
 class SegmentationConfig:
     TARGET_LESION = "ex"
+ 
     # paths
     root_dir = os.path.join("/kaggle", "input", "idriddata", "data", "IDRiD", "A. Segmentation")
-
+ 
     train_images_dir = os.path.join(root_dir, "1. Original Images", "a. Training Set")
     train_masks_dir = os.path.join(root_dir, "2. All Segmentation Groundtruths", "a. Training Set")
-
+ 
     val_images_dir = os.path.join(root_dir, "1. Original Images", "b. Testing Set")
     val_masks_dir = os.path.join(root_dir, "2. All Segmentation Groundtruths", "b. Testing Set")
-    
+ 
+    checkpoint_dir = "/kaggle/working"
+ 
     # model parameters
     backbone = "efficientnet-b3"
     pretrained = True
@@ -24,7 +27,8 @@ class SegmentationConfig:
     num_epochs = 150
     num_classes = 3
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+ 
     # data augmentation parameters
     clahe_probability = 1 
     grid_distortion_probability = 0.5
+ 
